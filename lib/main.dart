499ae42ed259c724e50116355aae640b03fb3682
dhs26 <dhs262003@gmail.com>
2022-06-11 21:45:08 +0530

    Splash Screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:spotify_api_app/screens/app.dart';
 
 void main(){
-  runApp( const MaterialApp(
+  runApp( MaterialApp(
     title: 'JetFit',
     debugShowCheckedModeBanner: false,
-    home: MyApp(),
+    // home: MyApp(),
+    home: AnimatedSplashScreen(
+            duration: 3000,
+            splash: 'assets/icon.png',
+            splashIconSize: double.infinity,
+            nextScreen: MyApp(),
+            splashTransition: SplashTransition.scaleTransition,
+          ) 
   ));
 }
