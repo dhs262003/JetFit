91572b5ebd195f31431d61348732ef3eb8a0123c
dhs26 <dhs262003@gmail.com>
2022-06-18 13:11:38 +0530

    Login page updated and firebase error solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'login/login_main.dart';
 
-void main(){
-  runApp( MaterialApp(
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  // Firebase.initializeApp(); error solved
+  runApp(const RunMainApp());
+}
+
+class RunMainApp extends StatelessWidget {
+  const RunMainApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
     title: 'JetFit',
     debugShowCheckedModeBanner: false,
     // home: MyApp(),
@@ -14,5 +26,6 @@ void main(){
             nextScreen: MyLoginApp(),
             splashTransition: SplashTransition.scaleTransition,
           ) 
-  ));
+  );
+  }
 }
